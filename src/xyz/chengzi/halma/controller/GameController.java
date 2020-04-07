bf061738a5856fefa17436f13a729098bdfddba8
NewbieOrange <NewbieOrange@users.noreply.github.com>
2020-04-07 21:36:00 +0800

    Initial commit

---
+++ src/xyz/chengzi/halma/controller/GameController.java
@@ -0,0 +1,72 @@
+package xyz.chengzi.halma.controller;
+
+import xyz.chengzi.halma.listener.InputListener;
+import xyz.chengzi.halma.model.ChessBoard;
+import xyz.chengzi.halma.model.ChessBoardLocation;
+import xyz.chengzi.halma.model.ChessPiece;
+import xyz.chengzi.halma.view.ChessBoardComponent;
+import xyz.chengzi.halma.view.ChessComponent;
+import xyz.chengzi.halma.view.SquareComponent;
+
+import java.awt.*;
+
+public class GameController implements InputListener {
+    private ChessBoardComponent view;
+    private ChessBoard model;
+
+    private ChessBoardLocation selectedLocation;
+    private Color currentPlayer;
+
+    public GameController(ChessBoardComponent chessBoardComponent, ChessBoard chessBoard) {
+        this.view = chessBoardComponent;
+        this.model = chessBoard;
+
+        this.currentPlayer = Color.RED;
+        view.registerListener(this);
+        model.registerListener(view);
+        model.placeInitialPieces();
+    }
+
+    public ChessBoardLocation getSelectedLocation() {
+        return selectedLocation;
+    }
+
+    public void setSelectedLocation(ChessBoardLocation location) {
+        this.selectedLocation = location;
+    }
+
+    public void resetSelectedLocation() {
+        setSelectedLocation(null);
+    }
+
+    public boolean hasSelectedLocation() {
+        return selectedLocation != null;
+    }
+
+    public Color nextPlayer() {
+        return currentPlayer = currentPlayer == Color.RED ? Color.GREEN : Color.RED;
+    }
+
+    @Override
+    public void onPlayerClickSquare(ChessBoardLocation location, SquareComponent component) {
+        if (hasSelectedLocation() && model.isValidMove(getSelectedLocation(), location)) {
+            model.moveChessPiece(selectedLocation, location);
+            resetSelectedLocation();
+            nextPlayer();
+        }
+    }
+
+    @Override
+    public void onPlayerClickChessPiece(ChessBoardLocation location, ChessComponent component) {
+        ChessPiece piece = model.getChessPieceAt(location);
+        if (piece.getColor() == currentPlayer && (!hasSelectedLocation() || location.equals(getSelectedLocation()))) {
+            if (!hasSelectedLocation()) {
+                setSelectedLocation(location);
+            } else {
+                resetSelectedLocation();
+            }
+            component.setSelected(!component.isSelected());
+            component.repaint();
+        }
+    }
+}
---
+++ src/xyz/chengzi/halma/model/ChessBoard.java
@@ -0,0 +1,93 @@
+package xyz.chengzi.halma.model;
+
+import xyz.chengzi.halma.listener.GameListener;
+import xyz.chengzi.halma.listener.Listenable;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChessBoard implements Listenable<GameListener> {
+    private List<GameListener> listenerList = new ArrayList<>();
+    private Square[][] grid;
+    private int dimension;
+
+    public ChessBoard(int dimension) {
+        this.grid = new Square[dimension][dimension];
+        this.dimension = dimension;
+
+        initGrid();
+    }
+
+    private void initGrid() {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                grid[i][j] = new Square(new ChessBoardLocation(i, j));
+            }
+        }
+    }
+
+    public void placeInitialPieces() {
+        // TODO: This is only a demo implementation
+        grid[0][0].setPiece(new ChessPiece(Color.RED));
+        grid[0][1].setPiece(new ChessPiece(Color.RED));
+        grid[1][0].setPiece(new ChessPiece(Color.RED));
+        grid[dimension - 1][dimension - 1].setPiece(new ChessPiece(Color.GREEN));
+        grid[dimension - 1][dimension - 2].setPiece(new ChessPiece(Color.GREEN));
+        grid[dimension - 2][dimension - 1].setPiece(new ChessPiece(Color.GREEN));
+        listenerList.forEach(listener -> listener.onChessBoardReload(this));
+    }
+
+    public Square getGridAt(ChessBoardLocation location) {
+        return grid[location.getRow()][location.getColumn()];
+    }
+
+    public ChessPiece getChessPieceAt(ChessBoardLocation location) {
+        return getGridAt(location).getPiece();
+    }
+
+    public void setChessPieceAt(ChessBoardLocation location, ChessPiece piece) {
+        getGridAt(location).setPiece(piece);
+        listenerList.forEach(listener -> listener.onChessPiecePlace(location, piece));
+    }
+
+    public ChessPiece removeChessPieceAt(ChessBoardLocation location) {
+        ChessPiece piece = getGridAt(location).getPiece();
+        getGridAt(location).setPiece(null);
+        listenerList.forEach(listener -> listener.onChessPieceRemove(location));
+        return piece;
+    }
+
+    public void moveChessPiece(ChessBoardLocation src, ChessBoardLocation dest) {
+        if (!isValidMove(src, dest)) {
+            throw new IllegalArgumentException("Illegal halma move");
+        }
+        setChessPieceAt(dest, removeChessPieceAt(src));
+    }
+
+    public int getDimension() {
+        return dimension;
+    }
+
+    public boolean isValidMove(ChessBoardLocation src, ChessBoardLocation dest) {
+        if (getChessPieceAt(src) == null || getChessPieceAt(dest) != null) {
+            return false;
+        }
+        int srcRow = src.getRow(), srcCol = src.getColumn(), destRow = dest.getRow(), destCol = dest.getColumn();
+        int rowDistance = destRow - srcRow, colDistance = destCol - srcCol;
+        if (rowDistance != 0 && colDistance != 0 && Math.abs((double) rowDistance / colDistance) != 1.0) {
+            return false;
+        }
+        return Math.abs(rowDistance) <= 1 && Math.abs(colDistance) <= 1;
+    }
+
+    @Override
+    public void registerListener(GameListener listener) {
+        listenerList.add(listener);
+    }
+
+    @Override
+    public void unregisterListener(GameListener listener) {
+        listenerList.remove(listener);
+    }
+}
